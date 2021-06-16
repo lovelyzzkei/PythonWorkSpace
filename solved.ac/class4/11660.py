e085a6c908bf4fc5ad2249173d669cbdedb65e44
lovelyzzkei <march381@naver.com>
2021-06-16 19:48:54 +0900

    updates

---
+++ solved.ac/class4/11660.py
@@ -0,0 +1,32 @@
+import sys; read = sys.stdin.readline
+from collections import deque
+
+def bfs(y1, x1, y2, x2):
+    visited = [[False] * n for i in range(n)]
+    visited[y1][x1] = True
+    q = deque([[y1+1, 0]])
+    ret = graph[y1][x1]
+
+    while q:
+        cy, cx = q.popleft()
+
+        for i in range(4):
+            ny = cy + dy[i]; nx = cx + dx[i]
+            if y1<=ny<=y2 and x1<=nx<=x2 and not visited[ny][nx]:
+                visited[ny][nx] = True
+                ret += graph[ny][nx]
+                q.append([ny, nx])
+
+    return ret
+
+n, m = map(int, read().split())
+graph = [list(map(int, read().split())) for i in range(n)]
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
+ret = []
+for _ in range(m): 
+    y1, x1, y2, x2 = map(int, read().split())
+    ret.append(bfs(y1-1, x1-1, y2-1, x2-1))
+
+print('\n'.join(str(x) for x in ret))
