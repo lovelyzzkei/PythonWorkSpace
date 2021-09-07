902590be72c5d5b9e65b20c49dc86078a160ace8
lovelyzzkei <march11381@gmail.com>
2021-09-07 09:29:36 +0900

    updates

--- solved.ac/9376.py
+++ solved.ac/9376.py
@@ -20,14 +20,15 @@ for tc in range(t):
                     prisoner.append([i, j])
         prison.append(tmp)
 
-    d = [[INF for j in range(w)] for i in range(h)]
+    def onEdge(y, x):
+        return True if y==0 or y==h-1 or x==0 or x==w-1 else False
 
     def inMap(y, x):
         return 0<=y<h and 0<=x<w
 
     # 죄수자 2명에 대해서 다익스트라 실행 -> 경로에 존재하는 문의 개수와 경로를 저장
     def dijkstra(y, x):
-        q = [0, y, x, []]
+        q = [[0, y, x, []]]
         heapq.heapify(q)
         d[y][x] = 0
         ret = []
@@ -36,4 +37,38 @@ for tc in range(t):
             num_door, cy, cx, route = heapq.heappop(q)
             if d[cy][cx] < num_door:
                 continue
-            if 
+            if  onEdge(cy, cx):
+                ret.append([num_door, route])
+            
+            for dy, dx in (-1, 0), (0, 1), (1, 0), (0, -1):
+                ny=cy+dy; nx=cx+dx; tmp = route[:]
+                if inMap(ny, nx) and prison[ny][nx] != -1:
+                    cost = num_door + prison[ny][nx]
+                    if cost < d[ny][nx]:
+                        d[ny][nx] = cost
+                        if prison[ny][nx] == 1:
+                            tmp.append([ny, nx])
+                        heapq.heappush(q, [cost, ny, nx, tmp])
+
+        return ret
+    
+    prisoner1 = []; prisoner2 = []
+    d = [[INF for j in range(w)] for i in range(h)]
+    for t in dijkstra(prisoner[0][0], prisoner[0][1]):
+        prisoner1.append(t)
+    d = [[INF for j in range(w)] for i in range(h)]
+    for t in dijkstra(prisoner[1][0], prisoner[1][1]):
+        prisoner2.append(t)
+
+    # print(prisoner1)
+    # print(prisoner2)
+
+    ans = INF
+    for d1, r1 in prisoner1:
+        for d2, r2 in prisoner2:
+            tmp = r1.copy()
+            tmp.extend(r2)
+            ans = min(ans, len(list(set([tuple(item) for item in tmp]))))
+    ret.append(ans)
+
+print(' \n'.join(str(x) for x in ret))
--- solved.ac/test.py
+++ solved.ac/test.py
@@ -1,2 +1,4 @@
-a = "1 2"
-print(list(map(int, a.split())))
+a = [1, 2, 3]
+b = [2, 3, 4]
+D = [i for i, j in zip(a, b) if i == j]
+print(set(a).intersection(b))
