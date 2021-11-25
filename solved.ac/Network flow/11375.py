79b81d67bf7a1079048dcd1455999391a30b3302
lovelyzzkei <march381@naver.com>
2021-11-25 10:07:35 +0900

    11375 on processing

--- solved.ac/Network flow/11375.py
+++ solved.ac/Network flow/11375.py
@@ -9,40 +9,44 @@ c = [[0 for i in range(n+m+2)] for j in range(n+m+2)]
 f = [[0 for i in range(n+m+2)] for j in range(n+m+2)]
 a = {i:[] for i in range(n+m+2)}
 
-def dfs(start, end, flow, visited):
-    if visited[start]:
-        return 0
-    visited[start] = True
 
-    # sink 노드 도착 -> 탈출
-    if start == end:
-        return flow
-
-    for y in a[start]:
-        now_flow = c[start][y] - f[start][y]
-        if now_flow <= 0:
-            continue
-
-        result = dfs(y, end, now_flow, visited)
-        if result:
-            f[start][y] += result
-            f[y][start] -= result
-            return result
-
-    return 0
-
-def maxFlow(start, end):
-    result = 0  
+def maxFlow():
+    totalFlow = 0  
 
     while True:
-        visited = [False for i in range(n+m+2)]
-        flow = dfs(start, end, INF, visited)
-        if not flow:
-            break
-        result += flow
-    
-    return result
+        # 이전 노드를 저장할 parent 리스트 및 스택을 위한 deque
+        parent = [-1 for i in range(n+m+2)]
+        inQueue = [False for i in range(n+m+2)]
+        parent[0] = 0
+        q = deque([0])
 
+        # 스택에 아직 노드가 존재하고 sink에 도달하지 않았다면 
+        while len(q) != 0 and parent[n+m+1] == -1:
+            x = q.pop()
+            inQueue[x] = False
+            
+            # y에서 갈 수 있는 노드들을 스택에 저장
+            for y in a[x]:
+                if c[x][y] - f[x][y] > 0 and parent[y] == -1 and not inQueue[y]:
+                    parent[y] = x
+                    inQueue[y] = True
+                    q.append(y)
+                        
+        print(parent)
+        # loop가 끝나고 sink에 도달하지 못했다면 탈출
+        if parent[n+m+1] == -1:
+            break
+
+        i = n + m + 1
+        while i != 0:
+            f[parent[i]][i] += 1
+            f[i][parent[i]] -= 1
+            i = parent[i]
+
+        totalFlow += 1
+    
+    return totalFlow
+        
 
 # 직원: 1 ~ n, 일: n+1 ~ n+m 으로 encoding하여 저장
 for i in range(1, n+1):
@@ -64,5 +68,4 @@ for i in range(n+1, n+m+1):
     a[n+m+1].append(i)
     c[i][n+m+1] = 1
 
-
-print(maxFlow(0, n+m+1))
+print(maxFlow())
