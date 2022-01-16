a3d9ef67c1027ad75c7a5e2daf655209430bb00c
lovelyzzkei <march11381@gmail.com>
2022-01-16 10:53:20 +0900

    Delete cryptoauto

--- cryptoauto/test.py
+++
@@ -1,9 +0,0 @@
-import pyupbit
-
-access = "cYr06LCxWexxZ3CdUVx5YZUb8u8IH9Rtt3Ton0km"
-secret = "qJxEwGiaue0Y5zTqRQVytpwLFDCyzGXQ4goPLnXd"
-
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-XRP"))
-print(upbit.get_balance("KRW"))
